4ec6c155becf7a86c699ae982b993ce8c87374c8
Viktor Belov <viktor.belov.k@gmail.com>
2022-08-14 23:42:15 +0300

    sync

--- VegasScript/DeleteSpaceStartEnd.cs
+++ VegasScript/DeleteSpaceStartEnd.cs
@@ -6,18 +6,20 @@ namespace DeleteSpaceStartEnd
     {
         public void FromVegas(Vegas vegas) 
         {
-            Timecode DeleteSpaceEnd = new Timecode("00:00:00:20");
-            Timecode DeleteSpaceStart = new Timecode("00:00:00:3");
+            Timecode trimLength = new Timecode("00:00:00:20");
+            //Timecode trimLength = Timecode.FromMilliseconds(50000);
+            //Timecode trimLength = Timecode.FromSeconds(50000);
+            //Timecode trimLength = Timecode.FromFrames(50000);
+            //Timecode trimLength = Timecode.FromPositionString("00:00:00:50");
 
-           
             foreach (Track CurrentTrack in vegas.Project.Tracks)
                 if (CurrentTrack.Selected)
                     foreach (var CurrentEvent in CurrentTrack.Events)
-                        if (CurrentEvent.Selected && CurrentEvent.Length > (DeleteSpaceEnd + DeleteSpaceStart))
+                        if (CurrentEvent.Selected && CurrentEvent.Length > (trimLength + trimLength))
                         {
-                            CurrentEvent.ActiveTake.Offset += DeleteSpaceStart;
-                            CurrentEvent.Length -= DeleteSpaceEnd + DeleteSpaceStart;
-                            CurrentEvent.Start += DeleteSpaceStart;
+                            CurrentEvent.ActiveTake.Offset += trimLength;
+                            CurrentEvent.Length -= trimLength + trimLength;
+                            CurrentEvent.Start += trimLength;
                         }
         }
     }
