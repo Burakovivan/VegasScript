9634eee647a6b9d9edbd90bd65f28ed2a70bd51b
Viktor Belov <viktor.belov.k@gmail.com>
2024-01-19 19:32:07 +0200

    sync

---
+++ VegasScript/AddReverb.cs
@@ -0,0 +1,23 @@
+﻿using VegasWrapper = ScriptPortal.Vegas.Vegas;
+using ScriptPortal.Vegas; 
+
+namespace AddReverb
+{
+    public partial class EntryPoint
+    {
+        public void FromVegas(VegasWrapper vegas)
+        {
+
+            foreach (Track CurrentTrack in vegas.Project.Tracks)
+                if (CurrentTrack.Selected && CurrentTrack.IsAudio())
+                    foreach (var CurrentEvent in CurrentTrack.Events)
+                        if (CurrentEvent.Selected)
+                        {
+                            Effect reverbEffect = new Effect(vegas.PlugIns.FindChildByName("Reverb"));
+                            (CurrentEvent as AudioEvent).Effects.Add(reverbEffect);
+                            reverbEffect.Preset = "AABBCC";
+                        }
+
+        }
+    }
+}
